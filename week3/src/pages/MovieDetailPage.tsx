33464d6cdb30ec73e41a82d9e825b9b70f2bd03f
young0206 <157898232+young0206@users.noreply.github.com>
2025-04-02 23:34:07 +0900

    week3-3rd-fail

--- week3/src/pages/MovieDetailPage.tsx
+++ week3/src/pages/MovieDetailPage.tsx
@@ -1,10 +1,80 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Credit, Movie } from "../types/movie";
+import { LoadingSpinner } from "../components/LoadingSpinner";
 
-const MovieDetailPage = () => {
-  const params = useParams();
+export default function MovieDetailPage() {
+  const [credit, setCredit] = useState<Credit | null>(null);
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [isPending, setIsPending] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  console.log(params);
-  return <div>MovieDetailPage</div>;
-};
+  const { movieId } = useParams<{ movieId: string }>();
 
-export default MovieDetailPage;
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsPending(true);
+      setIsError(false);
+
+      try {
+        const details = await axios.get<Movie>(
+          `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+          {
+            headers: {
+              Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+            },
+          }
+        );
+
+        const credits = await axios.get<Credit>(
+          `https://api.themoviedb.org/3/movie/${movieId}/credits`,
+          {
+            headers: {
+              Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+            },
+          }
+        );
+
+        setMovie(details.data);
+        setCredit(credits.data);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    fetchData();
+  }, [movieId]);
+
+  if (isError) {
+    return (
+      <div>
+        <span className="text-red-500 text-2xl">에러가 발생했습니다.</span>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {isPending && (
+        <div className="flex items-center justify-center h-dvh">
+          <LoadingSpinner />
+        </div>
+      )}
+
+      <img
+        src={`https://image.tmdb.org/t/p/w200${movie?.poster_path}`}
+        alt={`${movie?.title}의 이미지`}
+        className=""
+      />
+      <div className=" justify-center items-center">
+        <h2 className="text-lg font-bold leading-snug">{movie?.title}</h2>
+        <p>{movie?.vote_average}</p>
+        <p>{movie?.release_date}</p>
+        <p>{movie?.overview}</p>
+      </div>
+    </>
+  );
+}
--- week3/src/types/movie.ts
+++ week3/src/types/movie.ts
@@ -21,3 +21,18 @@ export type MovieResponse = {
   total_pages: number;
   total_results: number;
 };
+
+export type Credit = {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+};
